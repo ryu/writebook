39acb908bbe7925c9c853b82b812cd06144cdc96
dhh <david@loudthinking.com>
2024-02-14 17:09:37 -0800

    Leaf scopes

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -3,4 +3,6 @@ class Leaf < ApplicationRecord
   belongs_to :parent, class_name: "Leaf", required: false
 
   delegated_type :leafable, types: Leafable::TYPES, dependent: :destroy
+  scope :roots, -> { where(parent: nil) }
+  scope :with_leafables, -> { includes(:leafable) }
 end

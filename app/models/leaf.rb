54f8f96440f7154e91d28d2ff2dc0b8bcbcae883
dhh <david@loudthinking.com>
2024-02-14 16:40:05 -0800

    Parent is self-referential and optional

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -1,6 +1,6 @@
 class Leaf < ApplicationRecord
   belongs_to :book
-  belongs_to :parent
+  belongs_to :parent, class_name: "Leaf", required: false
 
   delegated_type :leafable, types: Leafable::TYPES, dependent: :destroy
 end

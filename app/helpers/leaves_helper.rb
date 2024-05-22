b5504df4d24acad84758c6efd8f82962d082dadf
Jason Zimdars <107673+jzimdars@users.noreply.github.com>
2024-05-22 17:13:58 -0500

    Introduce (messy) helper to open empty leaves for editing

--- app/helpers/leaves_helper.rb
+++ app/helpers/leaves_helper.rb
@@ -6,4 +6,20 @@ module LeavesHelper
       reading_tracker_leaf_id_value: leaf.id
     }, **options, &block
   end
+
+  def show_or_edit_leafable_path(leaf)
+    if leaf.book.editable?
+      if leaf.leafable.is_a?(Page) && leaf.leafable.body.to_html.empty?
+        edit_leafable_path(leaf)
+      elsif leaf.leafable.is_a?(Section) && leaf.title == "Section"
+        edit_leafable_path(leaf)
+      elsif leaf.leafable.is_a?(Picture) && !leaf.leafable.image.attached?
+        edit_leafable_path(leaf)
+      else
+        leafable_path(leaf)
+      end
+    else
+      leafable_path(leaf)
+    end
+  end
 end

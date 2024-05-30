f254ebd9f4415909d4bd37a075795ade6a2e894f
Alberto Fernández-Capel <afcapel@gmail.com>
2024-05-30 16:33:25 +0100

    Wire up lightbox for images
    Ref. https://3.basecamp.com/2914079/buckets/36389984/todos/7424593818

---
+++ lib/markdown_renderer.rb
@@ -0,0 +1,14 @@
+require "rouge/plugins/redcarpet"
+
+class MarkdownRenderer < Redcarpet::Render::HTML
+  include Rouge::Plugins::Redcarpet
+
+  def self.build_with_defaults
+    renderer = MarkdownRenderer.new(ActionText::Markdown::DEFAULT_RENDERER_OPTIONS)
+    Redcarpet::Markdown.new(renderer, ActionText::Markdown::DEFAULT_MARKDOWN_EXTENSIONS)
+  end
+
+  def image(url, title, alt_text)
+    %(<img src="#{url}" alt="#{alt_text}" title="#{title}" data-action="lightbox#open:prevent" data-lightbox-target="image" data-lightbox-url-value="#{url}?disposition=attachment" href="#{url}">)
+  end
+end

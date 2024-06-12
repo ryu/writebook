0e67817e842cd6fb5d65658da685751112cbec21
Kevin McConnell <kevin@37signals.com>
2024-06-12 13:46:27 +0100

    Inline pointless concern

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -1,11 +1,13 @@
 class Leaf < ApplicationRecord
-  include Editable, Positionable, Status
+  include Editable, Positionable
 
   belongs_to :book, touch: true
   positioned_within :book, association: :leaves
-
   delegated_type :leafable, types: Leafable::TYPES, dependent: :destroy
 
+  enum :status, %w[ draft published trashed ].index_by(&:itself), default: :draft
+
+  scope :active, -> { where.not(status: :trashed) }
   scope :with_leafables, -> { includes(:leafable) }
 
   def public_param
--- app/models/leaf/status.rb
+++
@@ -1,9 +0,0 @@
-module Leaf::Status
-  extend ActiveSupport::Concern
-
-  included do
-    enum :status, %w[ draft published trashed ].index_by(&:itself), default: :draft
-
-    scope :active, -> { where.not(status: :trashed) }
-  end
-end

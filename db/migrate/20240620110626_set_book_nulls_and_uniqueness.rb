702d938b8e073aa63f6bbd50f7b628e970374c9a
Kevin McConnell <kevin@37signals.com>
2024-06-20 15:22:03 +0100

    Prevent missing slugs breaking the migration

--- db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
+++ db/migrate/20240620110626_set_book_nulls_and_uniqueness.rb
@@ -1,5 +1,7 @@
 class SetBookNullsAndUniqueness < ActiveRecord::Migration[8.0]
   def change
+    execute "update books set slug = id where slug is null"
+
     change_column_null :books, :title, false
     change_column_null :books, :slug, false
 

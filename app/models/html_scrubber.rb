d8ba1f81009e14f16b4dcc27c5d6a05a7203fef8
Jason Zimdars <jz@37signals.com>
2024-07-01 14:03:21 -0500

    Allow `summary` which is a child of `details`
    Also add support for `source` which is used with `video`

--- app/models/html_scrubber.rb
+++ app/models/html_scrubber.rb
@@ -2,7 +2,7 @@ class HtmlScrubber < Rails::Html::PermitScrubber
   def initialize
     super
     self.tags = Rails::Html::WhiteListSanitizer.allowed_tags + %w[
-      audio details iframe options table tbody td th thead tr video
+      audio details summary iframe options table tbody td th thead tr video source
     ]
   end
 end

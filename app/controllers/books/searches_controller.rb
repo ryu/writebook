4bcb5e64687d88c59257067d5b416a6533ac4b95
Kevin McConnell <kevin@37signals.com>
2024-10-05 11:10:52 +0100

    Limit search results

--- app/controllers/books/searches_controller.rb
+++ app/controllers/books/searches_controller.rb
@@ -2,7 +2,7 @@ class Books::SearchesController < ApplicationController
   include BookScoped
 
   def create
-    @pages = book_pages.search_in(params[:search])
+    @pages = book_pages.search_in(params[:search]).limit(50)
   end
 
   private
